b8c3afd33a4d62edddc47a0ef6f5cbee1d7ffe59
andrearanica <andrearanica2004@gmail.com>
2023-01-15 15:01:10 +0100

    🚧 Continue search UI

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function Search () {
 
     const [research, setResearch] = useState('')
     const [result, setResult] = useState({})
+    const [parks, setParks] = useState([])
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8080/info/parks')
+        .then(res => setParks(res.data.result))
+    }, [])
 
     function searchAnimal (e) {
         e.preventDefault()
@@ -12,21 +18,24 @@ function Search () {
         .then(res => {
             if (res.data.result !== 'Animal not found') {
                 setResult(res.data.result)
-            } else {
-                alert(`Nessun animale ha l'id ${ research }`)
             }
-            console.log(res)
         })
+        .catch(setResult({}))
     }
 
     return (
         <div className="container my-4" id="divTitle">
             <h1>🔎 Ricerca</h1>
+            <select className="form-control my-1">
+                {
+                    parks.map(p => <option>{ p.nome_parco }</option>)
+                }
+            </select>
             <form role="search" onSubmit={ searchAnimal }>
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" onChange={ e => setResearch(e.target.value) } />
-                <button class="btn btn-outline-success my-1" style={{ color: 'white' }} type="submit">Cerca per ID</button>
+                <input class="form-control" type="search" placeholder="Cerca" aria-label="Search" onChange={ e => setResearch(e.target.value) } />
+                <button class="btn bg-light my-1" type="submit">Cerca per ID</button>
             </form>
-            { result ? <p>{ result.dataDiNascita }</p> : null }
+            { result.dataDiNascita ? <p>{ result.dataDiNascita }</p> : null }
         </div>
     )
 
