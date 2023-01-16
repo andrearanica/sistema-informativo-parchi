5bda6dcfd9172fd67f302655a605272509d8a5f5
Andrea Ranica <andrearanica2004@gmail.com>
2023-01-16 09:35:08 +0100

    💄 Navbar look

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,19 +2,19 @@ import { Link } from 'react-router-dom'
 
 function Navbar () {
     return (
-      <nav class="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: '#1E6F5C' }}>
-        <div class="container-fluid">
-          <Link class="navbar-brand" to="/"><b>SINP</b></Link>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: '#1E6F5C' }}>
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/"><b>SIP</b></Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
               </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/search">Cerca animali e parchi</Link>
+              <li className="nav-item">
+                <Link className="nav-link" to="/search">Cerca animali e parchi</Link>
               </li>
             </ul>
           </div>
